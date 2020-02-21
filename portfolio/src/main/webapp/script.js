45d015459a059bce482271f302e0e53d8832495d
gracieorozco <gracieorozco@csu.fullerton.edu>
2020-02-20 22:31:03 -0800

    Updated image and javascript function + created background, project, links, image gallery, and blog post sections

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -16,13 +16,6 @@
  * Adds a random greeting to the page.
  */
 function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
-
-  // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
-
-  // Add it to the page.
   const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
+  greetingContainer.innerText = "I have been to over 30 states in the United States.";
 }
